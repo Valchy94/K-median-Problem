ddebbd0979feb19c9f73cc2a1c10c67dfd5c97d4
Valchy94 <valentina.gnamus@gmail.com>
2017-02-07 22:13:42 +0100

    Update Forward_Greedy.py

--- Forward_Greedy.py
+++ Forward_Greedy.py
@@ -4,9 +4,10 @@
 
 #Najprej potrebujemo manhattansko razdaljo:
 def razdalja(x,y):
-    if type(x) is int or type(y) is int:
-        return False
-    elif len(x) != len(y):
+    print(x, y)
+    if type(x) is not tuple or type(y) is not tuple:
+        return abs(y - x)
+    if len(x) != len(y):
         return False
     else:
         n=len(x)
@@ -17,12 +18,12 @@ def razdalja(x,y):
 #Zdaj definiramo funkcijo cena:
 def cena(S, tocke):
     v=0
-    for i in tocke:          #gremo po vseh točkah gledat v kateri center bi najbolj pasale
-        a=razdalja(i, S[0])  #nastavila sem prvo vrednost a
+    for t in tocke:          #gremo po vseh točkah gledat v kateri center bi najbolj pasale
+        a=razdalja(t, S[0])  #nastavila sem prvo vrednost a
         j=0
         while j<len(S):
-            if razdalja(i,j) < a:
-                a=razdalja(i,j)
+            if razdalja(t,S[j]) < a:
+                a=razdalja(t,S[j])
             j+=1
         v+=a
     return v
@@ -34,13 +35,13 @@ def fw_greedy(tocke, k):
         a=cena(S+[tocke[0]], tocke[1:])   #nastavimo vrednost cene, če v S dodamo prvo točko
         j=0
         while j<len(tocke):
-            S1 = S.append(tocke[j])
+            S1 = S+[tocke[j]]
             tocke1 = tocke[:j]+tocke[j+1:]
             if cena(S1, tocke1) < a:
                 a=cena(S1, tocke1)
                 i=j  #shranila sem indeks trenutnega najboljšega kandidata za center v i
             j+=1
-        S=S.append(tocke[i])
+        S=S+[tocke[i]]
         tocke = tocke[:i] + tocke[i+1:]
     a=cena(S, tocke)
     print("Seznam centrov:",S,"Cena:", a)
